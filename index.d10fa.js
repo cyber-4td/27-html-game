55b1034281a5d4a0f1fb8b70d591b0fdd5e10bc8
t-nguyen <tunv@54vie.vn>
2026-01-28 12:33:52 +0700

    chore: hot fix UI

--- index.d10fa.js
+++ index.d10fa.js
@@ -1,4 +1,4 @@
-System.register(["./application.67237.js"], function (_export, _context) {
+System.register(["./application.79cbc.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
